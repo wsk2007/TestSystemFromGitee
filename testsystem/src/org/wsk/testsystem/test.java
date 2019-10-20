ef54d4bba145c4f3c398e38dcb529923c46fddc4
BuildTools <unconfigured@null.spigotmc.org>
2019-10-20 13:26:46 +0800

    0.1.1.06pre3

--- testsystem/src/org/wsk/testsystem/test.java
+++ testsystem/src/org/wsk/testsystem/test.java
@@ -1,22 +1,31 @@
 package org.wsk.testsystem;
-
+import java.util.*;
 public class test {
 	
 	private String name;
 	private boolean israndom;
 	private qusetiongiver g;
 	private randomgiver rg;
-	
+	private boolean isstart;
+	private UUID u = new UUID(1 , 5);
+	private UUID testuuid;
 	public test(String tname, qusetiongiver tg) {
 		g = tg;
 		israndom = false;
 		name = tname;
+		isstart = false;
 	}
 	
 	public test(String tname, randomgiver trg) {
 		rg = trg;
 		israndom = true;
 		name = tname;
+		isstart = false;
+	}
+	
+	public void start() {
+		isstart = true;
+		testuuid = u.randomUUID();
 	}
 
 	public String getName() {
@@ -33,6 +42,14 @@ public class test {
 
 	public randomgiver getRd() {
 		return rg;
+	}
+
+	public boolean isIsstart() {
+		return isstart;
+	}
+
+	public UUID getTestuuid() {
+		return testuuid;
 	}
 
 }
