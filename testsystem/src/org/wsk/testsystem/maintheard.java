1f5b265ec286ffe21c6ca7688a19d44fdab4e427
BuildTools <unconfigured@null.spigotmc.org>
2019-11-03 11:46:58 +0800

    0.1.3.01pre3bugfix01

--- testsystem/src/org/wsk/testsystem/maintheard.java
+++ testsystem/src/org/wsk/testsystem/maintheard.java
@@ -2,53 +2,31 @@ package org.wsk.testsystem;
 import java.util.*;
 
 public class maintheard {
-	public void test(){
-		List<qusetion> qlist = new ArrayList<>();
+	public void test() throws TestSystemException{
 		qusetion q1 = new qusetion();
-		try {
-			q1.setawnser("q1");
-		} catch (TestSystemException e3) {
-			// TODO 自动生成的 catch 块
-			e3.printStackTrace();
-		}
+		q1.setawnser("123");
+		q1.setname("123");
 		q1.setcanawnserbeempty(true);
-		try {
-			q1.setname("q1");
-		} catch (TestSystemException e2) {
-			// TODO 自动生成的 catch 块
-			e2.printStackTrace();
-		}
-		q1.setNum(1);
-		q1.setIsmarked(true);
-		qusetion q2 = new qusetion();
-		try {
-			q2.setawnser("q2");
-		} catch (TestSystemException e1) {
-			// TODO 自动生成的 catch 块
-			e1.printStackTrace();
-		}
-		q2.setcanawnserbeempty(false);
-		try {
-			q2.setname("q2");
-		} catch (TestSystemException e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
-		}
-		q2.setNum(2);
-		q2.setIsmarked(false);
-		qlist.add(q1);
-		qlist.add(q2);
-		qusetionlist l = new qusetionlist("list");
-		l.addqusetion(qlist);
-		System.out.println(l.getqusetion(0).checkawnser());
-		qusetiongiver qg = new qusetiongiver(l);
-		testhelper t = new testhelper("testtest", qg);
-		t.start();
+	    qusetion q2 = new qusetion();
+	    q2.setawnser("456");
+	    q2.setname("456");
+	    q1.setcanawnserbeempty(false);
+	    qusetionlist ql = new qusetionlist("qlist");
+	    ql.addqusetion(q1);
+	    ql.addqusetion(q2);
+	    qusetiongiver qg = new qusetiongiver(ql);
+	    testhelper test1 = new testhelper("test", qg);
+	    test1.start();
 	}
 		
 
 	public static void main(String[] args) {
-		new maintheard().test();
+		try {
+			new maintheard().test();
+		} catch (TestSystemException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
 		// TODO 自动生成的方法存根
 
 	}
