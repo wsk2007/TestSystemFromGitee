26b8c0287107dd132b9173f1bfadc406262551fc
BuildTools <unconfigured@null.spigotmc.org>
2020-01-18 17:30:00 +0800

    TestSystem 1.0 Was Released!

--- testsystem/src/org/wsk/testsystem/maintheard.java
+++ testsystem/src/org/wsk/testsystem/maintheard.java
@@ -7,45 +7,65 @@ import java.util.*;
 
 public class maintheard implements Serializable{
 	static boolean debug;
-	public void test() {
-		qusetion q1 = new qusetion();
-		try {
-			q1.setname("123");
-			q1.setawnser("123");
-			q1.setcanawnserbeempty(true);
-		} catch (TestSystemException e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
-		}
-		qusetion q2 = new qusetion();
-		try {
-			q2.setname("456");
-			q2.setawnser("456");
-			q2.setcanawnserbeempty(true);
-		} catch (TestSystemException e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
+	static Scanner s;
+	public static void Write(){
+		List<qusetion> ql = new ArrayList<>();
+		s = new Scanner(System.in);
+		int i = 0;
+		for(;;) {
+			if (getinput("More qusetion?").equals("yes")) {
+				
+				
+			    ql.add(new qusetion());
+			    try {
+					ql.get(i).setname(getinput("Name:"));
+					ql.get(i).setawnser(getinput("Awnser:"));
+					ql.get(i).setcanawnserbeempty(true);
+				} catch (TestSystemException e) {
+					// TODO 自动生成的 catch 块
+					e.printStackTrace();
+				}
+			    i++;
+			}else {
+				break;
+			}
+				
+			
+			
 		}
 		qusetionlist list = new qusetionlist("l1");
-		list.addqusetion(q1);
-		list.addqusetion(q2);
+		for(qusetion q : ql) {
+			list.addqusetion(q);
+		}
 		qusetiongiver qg = new qusetiongiver(list);
-		testhelper th = new testhelper("th1", qg);
+        testhelper th = new testhelper(getinput("Test name:"), qg);
+		System.out.println("Please open debug mode and do this test");
 		th.start();
+		
+		
+		
+		
 		
 				
 	}
-		
-
-	public static void main(String[] args) {
-		if (args.length == 0) {}
-		else {
-			if (args[0].equals("debug")){
-				debug = true;
-			}
-		}
+	
+	private static String getinput(String inputtext) {
+		String input = null;
+		System.out.print(inputtext);
 		try {
-			FileInputStream ifs = new FileInputStream(args[1]);
+		    
+		    
+		    input = s.next();
+		    
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return input;
+	}
+	
+	public static void Read(String l) {
+		try {
+			FileInputStream ifs = new FileInputStream(l);
 			ObjectInputStream ios = new ObjectInputStream(ifs);
 			Object o = ios.readObject();
 			testhelper test = (testhelper) o;
@@ -54,10 +74,35 @@ public class maintheard implements Serializable{
 		} catch (Exception e) {
 			// TODO 自动生成的 catch 块
 			e.printStackTrace();
-		}
+	}
+	}
 		
 		
 
-	}
+		public static void main(String[] args) {
+		if (args.length == 0) {}
+		else {
+			if (args[0].equals("debug")){
+				debug = true;
+			}
+		}
+		if(args[1].equals("read")) {
+			Read(args[2]);
+			
+		}
+		if(args[1].equals("write")) {
+			Write();
+			
+			
+		}
+		
+		
+		}
+		
+		
+		
+		
+
+	
 
 }
