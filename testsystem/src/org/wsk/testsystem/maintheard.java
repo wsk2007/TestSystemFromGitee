92d9b6729c6b98723bff0352d9cfd0fafcdc2fa8
BuildTools <unconfigured@null.spigotmc.org>
2020-01-18 11:44:45 +0800

    0.2

--- testsystem/src/org/wsk/testsystem/maintheard.java
+++ testsystem/src/org/wsk/testsystem/maintheard.java
@@ -1,24 +1,39 @@
 package org.wsk.testsystem;
-import java.io.Serializable;
+
+import java.io.*;
 import java.util.*;
+
+
 
 public class maintheard implements Serializable{
 	static boolean debug;
-	public void test() throws TestSystemException{
+	public void test() {
 		qusetion q1 = new qusetion();
-		q1.setawnser("123");
-		q1.setname("123");
-		q1.setcanawnserbeempty(true);
-	    qusetion q2 = new qusetion();
-	    q2.setawnser("456");
-	    q2.setname("456");
-	    q2.setcanawnserbeempty(false);
-	    qusetionlist ql = new qusetionlist("qlist");
-	    ql.addqusetion(q1);
-	    ql.addqusetion(q2);
-	    qusetiongiver qg = new qusetiongiver(ql);
-	    testhelper test1 = new testhelper("test", qg);
-	    test1.start();
+		try {
+			q1.setname("123");
+			q1.setawnser("123");
+			q1.setcanawnserbeempty(true);
+		} catch (TestSystemException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		qusetion q2 = new qusetion();
+		try {
+			q2.setname("456");
+			q2.setawnser("456");
+			q2.setcanawnserbeempty(true);
+		} catch (TestSystemException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		qusetionlist list = new qusetionlist("l1");
+		list.addqusetion(q1);
+		list.addqusetion(q2);
+		qusetiongiver qg = new qusetiongiver(list);
+		testhelper th = new testhelper("th1", qg);
+		th.start();
+		
+				
 	}
 		
 
@@ -30,12 +45,18 @@ public class maintheard implements Serializable{
 			}
 		}
 		try {
-			new maintheard().test();
-		} catch (TestSystemException e) {
+			FileInputStream ifs = new FileInputStream(args[1]);
+			ObjectInputStream ios = new ObjectInputStream(ifs);
+			Object o = ios.readObject();
+			testhelper test = (testhelper) o;
+			ios.close();
+			test.start();
+		} catch (Exception e) {
 			// TODO 自动生成的 catch 块
 			e.printStackTrace();
 		}
-		// TODO 自动生成的方法存根
+		
+		
 
 	}
 
